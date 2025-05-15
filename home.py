26c96530542a819496920ca7194a21f3335ad538
Stevenpm25 <gspena51@ucatolica.edu.co>
2025-05-15 03:01:14 -0500

    Adding html

--- home.py
+++ home.py
@@ -8,3 +8,16 @@ router = APIRouter()
 @router.get("/", response_class=HTMLResponse)
 async def read_home(request: Request):
     return templates.TemplateResponse("Niña.html", {"request": request})
+@router.get("/Html", response_class=HTMLResponse)
+async def pure_html():
+    html_content="""
+    <html>
+        <head>
+        <title>Niñagorda</title>
+        </head>
+        <body>
+        <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content)
